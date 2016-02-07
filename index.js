22be6aa60ed49428af2e79811b8e9e9e3516337d
John Grishin <r@exah.ru>
2016-02-07 23:49:03 +0300

    Synchronous SVGO transformer

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var Promise = require('promise');
 var extend = require('extend-shallow');
 
 exports.name = 'svgo';
+exports.inputFormats = ['svg'];
 exports.outputFormat = 'svg';
 
 exports.renderAsync = function (str, options, locals) {
@@ -12,11 +13,24 @@ exports.renderAsync = function (str, options, locals) {
     var svgo = new Svgo(extend({}, options, locals));
     svgo.optimize(str, function (result) {
       if (result.data) {
-        return resolve(result['data']);
+        return resolve(result.data);
       }
       else {
         reject(result)
       }
     });
   });
+};
+
+exports.render = function (str, options, locals) {
+  var svgo = new Svgo(extend({}, options, locals));
+  var data;
+
+  svgo.optimize(str, function (result) {
+    data = result.data
+  });
+
+  while (data === undefined) { /* waiting for data */ };
+
+  return data;
 };
